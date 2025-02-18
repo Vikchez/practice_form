75b4e7887e635ece7f2c94d26a73dfd1dc5e6cb5
Viktoria Shteinbah <hteinbah2015@gmail.com>
2025-02-18 13:20:47 +0300

    add practice form - registration

---
+++ src/test/java/demoqa/PracticeFormTest.java
@@ -0,0 +1,65 @@
+package demoqa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class PracticeFormTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "3024x1964";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @Test
+    void successfulSearchTest() {
+        String firstName = "Viktoria";
+        String lastName = "Shteinbah";
+        String email = "Shteinbah@mail.ru";
+        String phoneNumber = "8911456256";
+
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Practice Form"));
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        $("#genterWrapper").$(byText("Female")).click();
+        $("#userNumber").setValue(phoneNumber);
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("November");
+        $(".react-datepicker__year-select").selectOption("1998");
+        $(".react-datepicker__day--013").click();
+        $("#subjectsInput").setValue("en").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("img/cat.jpg");
+        $("#currentAddress").setValue("СПБ,ул.Планерная д.2 кв.36");
+        $("#state").scrollIntoView(true).click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Noida")).click();
+        $("#submit").click();
+
+        $(".modal-content").should(appear);
+        $(".modal-content").shouldHave(text("Thanks for submitting the form"));
+        $(".modal-body").shouldHave(
+                text(firstName),
+                text(lastName),
+                text(email),
+                text("Female"),
+                text(phoneNumber),
+                text("13 November,1998"),
+                text("English"),
+                text("Sports"),
+                text("cat.jpg"),
+                text("СПБ,ул.Планерная д.2 кв.36"),
+                text("NCR Noida")
+        );
+    }
+}
